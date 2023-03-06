b4e8615e59d17092c01fc588d0913519c205eb65
AH400796 <A.Hokhman@gmail.com>
2023-03-06 23:25:41 +0200

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -70,7 +70,7 @@ export default class App extends Component {
           }
           console.log(error.config);
         })
-        .finally(this.toggleIsLoading(), setTimeout(smoothImagesScroll, 1250));
+        .finally(this.toggleIsLoading(), smoothImagesScroll());
     }
   }
 
